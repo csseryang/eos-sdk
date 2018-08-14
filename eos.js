9558c60410683a9cd0b39291ba8df1e8d699cd8c
MakeBigNews <qding1990@gmail.com>
2018-08-13 21:05:15 -0700

    Remove callback

--- eos.js
+++ eos.js
@@ -10,42 +10,34 @@ const config = {
   }
 }
 
-function log(error, result) {
-	if (error !== null) {
-		console.log(error)
-	} else {
-		console.log(result)
-	}
-}
-
 // Generate random private key/ public key pair
-function random_key(callback=log) {
+function random_key() {
 	ecc.randomKey().then(pvk => {		
 		var puk = ecc.privateToPublic(pvk);
-		callback(null, JSON.stringify({"privateKey": pvk, "publicKey": puk}))
+		console.log(JSON.stringify({"privateKey": pvk, "publicKey": puk}))
 	}).catch(error=>{
-		callback(JSON.stringify({'error': error}))
+		console.log(JSON.stringify({'error': error}))
 	})
 }
 
 // Generate public key from private key
 function pvk_to_puk(pvk) {
 	var puk = ecc.privateToPublic(pvk);
-	callback(null, JSON.stringify({"publicKey": puk}));
+	console.log(null, JSON.stringify({"publicKey": puk}));
 }
 
 // Using privateKey to sign any input string
-function sign(s, pvk, callback=log) {
+function sign(s, pvk) {
 	try {
 		var signature = ecc.sign(s, pvk);
-		callback(null, JSON.stringify({"signature": signature}))
+		console.log(JSON.stringify({"signature": signature}))
 	} catch(error) {
-		callback(JSON.stringify({'error': error.message}))
+		console.log(JSON.stringify({'error': error.message}))
 	}
 }
 
 // Get account information from EOS 
-function get_account2(name) {
+function get_account(name) {
 	const eos = Eos(config)
 	eos.getAccount(name).then(result=>{
 		console.log(JSON.stringify({'account': result}))
@@ -54,20 +46,10 @@ function get_account2(name) {
 	})
 }
 
-// Get account information from EOS 
-function get_account(name, callback=log) {
-	const eos = Eos(config)
-	eos.getAccount(name).then(result=>{
-		callback(null, JSON.stringify({'account': result}))
-	}).catch(error=>{
-		callback(JSON.stringify({'error': error}))
-	})
-}
-
 // Get account balance from contract issuer
 // code is smart contract name
 // e.g. get_balance('mhwb3kzafoxg')
-function get_balance(name, code='eosio.token', callback=log) {
+function get_balance(name, code='eosio.token') {
 	const eos = Eos(config)
 	eos.getTableRows({
 	  "scope": name,
@@ -75,33 +57,33 @@ function get_balance(name, code='eosio.token', callback=log) {
 	  "table": "accounts",
 	  "json": true
 	}).then(balance=>{
-		callback(null, JSON.stringify({'balance': balance.rows}));
+		console.log(JSON.stringify({'balance': balance.rows}));
 	}).catch(error=>{
-		callback(JSON.stringify({'error': error}))
+		console.log(JSON.stringify({'error': error}))
 	})
 }
 
 // Get account names by public key
 // e.g. get_key_accounts('EOS5FxA3PnsnUu1prJRuqFKDXuQFZkEDjC3XudPUhfxfwooSfDYdr')
-function get_key_accounts(puk, callback=log) {
+function get_key_accounts(puk) {
 	const eos = Eos(config)
 	eos.getKeyAccounts(puk).then(names=>{
-		callback(null, JSON.stringify({'names': names}));
+		console.log(JSON.stringify({'names': names}));
 
 	}).catch(error=>{
-		callback(JSON.stringify({'error': error}))
+		console.log(JSON.stringify({'error': error}))
 	})
 }
 
 // get currency info by symbol
 // code is smart contract name
 // e.g. get_currency_stats('SYS')
-function get_currency_stats(symbol, code='eosio.token', callback=log) {
+function get_currency_stats(symbol, code='eosio.token') {
 	const eos = Eos(config)
 	eos.getCurrencyStats(code, symbol).then(stats=>{
-		callback(null, JSON.stringify({'stats': stats}));
+		console.log(JSON.stringify({'stats': stats}));
 	}).catch(error=>{
-		callback(JSON.stringify({'error': error}))
+		console.log(JSON.stringify({'error': error}))
 	})
 }
 
@@ -119,11 +101,11 @@ function _add_pvk(pvk) {
 // e.g. SYS's max_supply = 10000.0000, then '1.0000 SYS' is legal while '1 SYS' is illegal
 // Get token info by function get_token_info()
 // e.g. transfer('from_user', 'to_user', '3.1415 EZPT', 'sample memo', private_key')
-function transfer(from, to, amount, memo, pvk, callback=log) {
+function transfer(from, to, amount, memo, pvk) {
 	const eos = Eos(_add_pvk(pvk))
 	eos.transfer(from, to, amount, memo).then(result=>{
-	  callback(null, {'result':result})
+	  console.log({'result':result})
 	}).catch(error=>{
-	  callback(JSON.stringify({'error': error.message}))
+	  console.log(JSON.stringify({'error': error.message}))
 	})
 }
