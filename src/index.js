55bed1771e4b5f1049f8640bcab824ce20fedcf9
qding <qding1990@gmail.com>
2018-08-17 15:03:44 -0700

    Added travis.yml

--- src/index.js
+++ src/index.js
@@ -6,8 +6,16 @@ const settings = require('./config');
 
 const config = settings.config;
 
+function log(error, result) {
+    if (result !== undefined) {
+        console.log(result);
+    } else {
+        console.log(result);
+    }
+}
+
 // Generate random private key/ public key pair
-function random_key(callback = console.log) {
+function random_key(callback = log) {
     ecc.randomKey().then(pvk => {
         const puk = ecc.privateToPublic(pvk);
         callback(null, JSON.stringify({"privateKey": pvk, "publicKey": puk}));
@@ -17,14 +25,14 @@ function random_key(callback = console.log) {
 }
 
 // Generate public key from private key
-function pvk_to_puk(pvk, callback = console.log) {
+function pvk_to_puk(pvk, callback = log) {
     const puk = ecc.privateToPublic(pvk);
     callback(JSON.stringify({"publicKey": puk}));
 }
 
 
 // Using privateKey to sign any input string
-function sign(s, pvk, callback = console.log) {
+function sign(s, pvk, callback = log) {
     try {
         const signature = ecc.sign(s, pvk);
         callback(null, JSON.stringify({"signature": signature}));
@@ -45,7 +53,7 @@ function _config(pvk) {
 }
 
 // Get account information from EOS
-function get_account(name, callback = console.log) {
+function get_account(name, callback = log) {
     const eos = Eos(_config());
     eos.getAccount(name).then(result => {
         callback(null, JSON.stringify({'account': result}))
@@ -58,7 +66,7 @@ function get_account(name, callback = console.log) {
 // Get account balance from contract issuer
 // code is smart contract name
 // e.g. get_balance('mhwb3kzafoxg')
-function get_balance(name, code = 'eosio.token', callback = console.log) {
+function get_balance(name, code = 'eosio.token', callback = log) {
     const eos = Eos(_config());
     eos.getTableRows({
         "scope": name,
@@ -75,12 +83,12 @@ function get_balance(name, code = 'eosio.token', callback = console.log) {
 
 // Get account names by public key
 // e.g. get_key_accounts('EOS5FxA3PnsnUu1prJRuqFKDXuQFZkEDjC3XudPUhfxfwooSfDYdr')
-function get_key_accounts(puk) {
+function get_key_accounts(puk, callback = log) {
     const eos = Eos(_config());
     eos.getKeyAccounts(puk).then(names => {
-        console.log(JSON.stringify({'names': names}));
+        callback(null, JSON.stringify({'names': names}));
     }).catch(error => {
-        console.log(JSON.stringify({'error': error}));
+        callback(JSON.stringify({'error': error}));
     })
 }
 
@@ -88,12 +96,12 @@ function get_key_accounts(puk) {
 // get currency info by symbol
 // code is smart contract name
 // e.g. get_currency_stats('SYS')
-function get_currency_stats(symbol, code = 'eosio.token') {
+function get_currency_stats(symbol, code = 'eosio.token', callback = log) {
     const eos = Eos(_config());
     eos.getCurrencyStats(code, symbol).then(stats => {
-        console.log(JSON.stringify({'stats': stats}));
+        callback(null, JSON.stringify({'stats': stats}));
     }).catch(error => {
-        console.log(JSON.stringify({'error': error}));
+        callback(JSON.stringify({'error': error}));
     })
 }
 
@@ -103,15 +111,15 @@ function get_currency_stats(symbol, code = 'eosio.token') {
 // e.g. SYS's max_supply = 10000.0000, then '1.0000 SYS' is legal while '1 SYS' is illegal
 // Get token info by function get_token_info()
 // e.g. transfer('from_user', 'to_user', '3.1415 EZPT', 'sample memo', private_key')
-function transfer(from, to, amount, memo, pvk) {
+function transfer(from, to, amount, memo, pvk, callback = log) {
     const eos = Eos(_config(pvk));
     eos.transfer(from, to, amount, memo).then(result => {
-        console.log({'result': result});
+        callback(null, {'result': result});
     }).catch(error => {
         try {
-            console.log(JSON.stringify({'error': JSON.parse(error).message}));
+            callback(JSON.stringify({'error': JSON.parse(error).message}));
         } catch (e) {
-            console.log(JSON.stringify({'error': error.message}));
+            callback(JSON.stringify({'error': error.message}));
         }
     })
 }
